c93c489be8e45452fa9e4c08b80eaa49d663328c
NerdOfCode <NerdOfCode@users.noreply.github.com>
2017-09-03 21:11:47 -0500

    Update Game.py
    Updated original code

--- Game.py
+++ Game.py
@@ -2,20 +2,30 @@ import os, sys, random
 
 money=0
 items=[]
+availableItems=["flashlight", "crowbar"]
 
 def shop(money):
   print("You have %i money(s)" % money + "\n")
   print("There are 5 available items you can purchase, \n Item #(1) Flashlight \n #(2) Crowbar \n #(3)")
   newItem = input("Which one would you like to purchase?: ")
-  
-  items.append(newItem)
+  x=0
+  #While length is less than the array
+  while x <= len(availableItems):
+    if x == int(newItem):
+      items.append(availableItems[x-1])
+    else:
+      x=x+1
   #When User buys a new item
   inventory(items)
   
+#For user inventory
 def inventory(items):
   itemsString=",".join(items)
-  print("You have: %s" % itemsString)
+  print("\n You have: %s" % itemsString + "\n")
   
 while True:
-  user_input=input("To visit the shop type \"shop\", for your inventory type \"inventory\", for all other options follow later directions ")
-  
+  user_input=input("To visit the shop type \"shop\",\n for your inventory type \"inventory\",\n for all other options follow later directions: ")
+  if user_input == "shop":
+    shop(money)
+  elif user_input == "inventory":
+    inventory(items)

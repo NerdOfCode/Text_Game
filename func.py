a6ced631b18e9c8b64e3385862cd8716974e0136
NerdOfLinux <golf@runningwind.com>
2017-10-07 10:10:42 -0500

    Shop has prices

--- func.py
+++ func.py
@@ -11,8 +11,6 @@ getLoc=0
 min_boundaries=[0,0]
 max_boundaries=[2,2]
 
-
-
 #Generate the treasure/and other treasure related variables
 treasurex=randint(1,2)
 treasurey=randint(1,2)
@@ -22,11 +20,12 @@ print("Hint: ",treasureLoc)
 max_earnings=150
 generate_money=randint(1,max_earnings)
 
-availableItems=["flashlight", "crowbar"]
+availableItems=["flashlight", "crowbar", "sword", "pistol"]
+availableItemPrices=["30", "50", "65", "250"]
 file="user.txt"
 
 #Function for saving inventory and money
-def save(inventory, money):
+def save(inventory):
         #If file does not exist create one
         if not os.path.isfile(file):
                 file_open=open(file, "a")
@@ -37,8 +36,6 @@ def save(inventory, money):
         file_open.close()
 
 def open_file():
-  #Make money available everywhere
-  global money
   if not os.path.isfile(file):
     file_open(file, "a")
 
@@ -48,32 +45,39 @@ def open_file():
             money=line.split("money: ", 1)
             money=int(money[1])
             print("money: ", money)
-
-
-def shop(money):
+#Check if there is enough money
+def isEnough(price):
+    if price <= money:
+        #Allow changing of global varaible money
+        global money
+        money=money-price
+        return True
+    else:
+        return False
+def shop():
   print("You have %i dollars" % money + "\n")
   #Split array into readable text
   splitAvailableItems=""
   for numVar in range(0, len(availableItems)):
         splitAvailableItems+="#%i) "%(numVar+1)+availableItems[numVar] + "\n"
   print("There are %i available items you can purchase, Item \n%s"%(len(availableItems), splitAvailableItems))
-  newItem = input("Which one would you like to purchase?: ")
+  newItem = raw_input("Which one would you like to purchase?: ")
   #If item is already in inventory
   x=0
   #While length is less than the array
-  while x <= len(availableItems):
+  for x in range(0, len(availableItems)):
     #If x is equal to the item the user entered
     if x == int(newItem):
         if availableItems[x-1] in items:
                 print("You already have that.")
                 return None
                 break
-
-        items.append(availableItems[x-1])
+        if isEnough(availableItemPrices[x-1]):
+            items.append(availableItems[x-1])
+        else:
+            print("Not enough money")
         #Exit loop
         break
-    else:
-      x=x+1
 #For user inventory
 def inventory(items):
   itemsString=",".join(items)
--- game.py
+++ game.py
@@ -4,7 +4,7 @@ while True:
   print("To visit the shop type \"shop\",\n for your inventory type \"inventory\",\n to make a move type \"move\", to save type \"save\"\n")
   user_input = raw_input("\n Action: ")
   if user_input.lower() == "shop":
-    shop(money)
+    shop()
   elif user_input.lower() == "inventory":
     inventory(items)
   elif user_input.lower() == "move":
